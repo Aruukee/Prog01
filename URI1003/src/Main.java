4267dbd36a23d38194eb192b6a7412315d6a6e22
Aruuke Usonalieva <usoanlieva_a@auca.kg>
2018-10-25 15:31:56 +0600

    URI1005

---
+++ URI1003/src/Main.java
@@ -0,0 +1,12 @@
+import com.sun.media.sound.SoftMidiAudioFileReader;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int=
+    }
+}
